9ac69b4994c06ac6b008e187c30976a980b59ec0
tahaerden <tahaerden@windowslive.com>
2020-08-27 15:22:36 +0300

    Added mat-tooltips to timeline bars and statuses. Added custom classes to the tooltips to support multiline.

--- projects/ngx-daily-gantt-chart/src/lib/ngx-daily-gantt-chart.module.ts
+++ projects/ngx-daily-gantt-chart/src/lib/ngx-daily-gantt-chart.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxDailyGanttChartComponent } from './ngx-daily-gantt-chart.component';
 
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 @NgModule({
   declarations: [NgxDailyGanttChartComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatTooltipModule],
   exports: [NgxDailyGanttChartComponent]
 })
 export class NgxDailyGanttChartModule {}
