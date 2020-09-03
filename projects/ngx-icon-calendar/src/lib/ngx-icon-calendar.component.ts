47bef328f37683f3900dcec239f5d32129c46cc1
tahaerden <tahaerden@windowslive.com>
2020-09-03 17:49:09 +0300

    ngx-icon-calendar: Added inputs to make widget work.

--- projects/ngx-icon-calendar/src/lib/ngx-icon-calendar.component.ts
+++ projects/ngx-icon-calendar/src/lib/ngx-icon-calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ngx-icon-calendar',
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['ngx-icon-calendar.component.scss']
 })
 export class NgxIconCalendarComponent implements OnInit {
+  @Input() events: any;
+  @Input() prevButtonText = '';
+  @Input() nextButtonText = '';
   iconSortArray = [];
   eventDates = [];
-  events = {};
   cells = [];
   today = new Date();
   currentMonth = this.today.getMonth();
@@ -199,6 +201,5 @@ export class NgxIconCalendarComponent implements OnInit {
         }
       }
     }
-    console.log(this.cells);
   }
 }
